bb7cac1010fe6d3de05b0f82c50413173aa72a0f
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-07-19 11:33:34 -0300

    feat: adicionando método de editar disciplina

--- escola-app/src/app/app.component.ts
+++ escola-app/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   selecionado:any;
   nome:any;
   descricao:any;
+  editando:any;
   
   disciplinas = [
     new Disciplina('Lingua Portuguesa', 'O objetivo é aprender português'),
@@ -27,17 +28,39 @@ export class AppComponent {
   }
 
   salvar() {
-    const d = new Disciplina(this.nome, this.descricao);
-    this.disciplinas.push(d);
-    this.nome;
-    this.descricao;
+    if (this.editando) {
+      this.editando.nome = this.nome;
+      this.editando.descricao = this.descricao;
+    } else {
+      const d = new Disciplina(this.nome, this.descricao);
+      this.disciplinas.push(d);
+    }
+    this.nome = null;
+    this.descricao = null;
+    this.editando = null;
   }
 
   excluir(disciplina:any) {
-    if (confirm('Tem certeza que deseja excluir a disciplina "'
-    + disciplina.nome + '"?')) {
-    const i = this.disciplinas.indexOf(disciplina);
-    this.disciplinas.splice(i, 1);
+    if (this.editando == disciplina) {
+      alert('Você não pode excluir uma disciplina que está editando');
+    } else {
+      if (confirm('Tem certeza que deseja excluir a disciplina "'
+          + disciplina.nome + '"?')) {
+        const i = this.disciplinas.indexOf(disciplina);
+        this.disciplinas.splice(i, 1);
+      }
     }
+  }
+
+  editar(disciplina:any) {
+    this.nome = disciplina.nome;
+    this.descricao = disciplina.descricao;
+    this.editando = disciplina;
+  }
+
+  cancelar() {
+    this.nome = null;
+    this.descricao = null;
+    this.editando = null;
   }
 }
